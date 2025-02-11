3ed9a4bf0e90e0f8019dd993bb777e2042bce64e
muhammadanwaystudent <195834534+muhammadanwaystudent@users.noreply.github.com>
2025-02-11 00:14:19 -0700

    add prefetch and robots txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import starlight from '@astrojs/starlight';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://smcd.cc',
+	prefetch: { prefetchAll: true, defaultStrategy: 'load' },
+	experimental: { clientPrerender: true },
 	integrations: [
 		starlight({
 			title: 'SMC Dawah',
